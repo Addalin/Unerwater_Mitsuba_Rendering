0bb53f20ffad4530d43b82dea3b2f1d28dbff45f
Addalin <adi.vainiger@gmail.com>
2017-02-12 20:04:36 +0200

    main.py - adding option to save results

--- Main.py
+++ Main.py
@@ -6,7 +6,9 @@ import scipy.io
 import nextBestViewLib as nbv
 import matplotlib.pyplot as plt
 import mitsubaWrapperLib as mitLib
-import miscGeometry as mgo  
+import miscGeometry as mgo
+
+from time import gmtime, strftime
     
 ## SET PARAMETERS:
 params = {}
@@ -15,31 +17,33 @@ params['camWidth']    = 400 # Image width
 params['camHeight']   = 400 # Image hight
 
 ## Set all file paths:
+base_path    = r'C:\Users\addalin\mitsuba_sim\3D_models'.replace('\\', '/')
+resultsPath = r'C:\Users\addalin\mitsuba_sim\sim_results'.replace('\\', '/') 
+
 scene_name = 'hetvol'#''cube_with_texture'##hetvol' ##'
-base_path    = r'C:/Users/addalin/mitsuba_sim/3D_models/'
-shape_filename   = base_path + scene_name + '/mitsuba/' + scene_name + '.serialized'
-boundsPLYPath = base_path + scene_name + '/' + 'bounds' + '.ply'
-screenPLYPath = base_path + scene_name + '/' + 'screen' + '.ply'
+shape_filename   = base_path + '/' + scene_name + '/mitsuba/' + scene_name + '.serialized'
+boundsPLYPath = base_path + '/' + scene_name + '/' + 'bounds' + '.ply'
+screenPLYPath = base_path + '/'  + scene_name + '/' + 'wideScreen' + '.ply'
                      
 ## PARAMETERS
-do_plot = 1
+do_plot = 0
 
 ## creating cameras on an arch path
-upDirection = np.array([0,0,1])
-target = np.array([0 , 0 , 0])
 numViews = 6
-radius = 3
+camsRadius = 3
 camsHeight = 0
 archAngleSize = 125
-#acrchCenter = 270  #TODO: change later to center direction = [0,-1,0]
-screenDirection = np.array([0, 1, 0]) 
-cams = mgo.createCamsCirc(numViews , radius , camsHeight , upDirection , target , archAngleSize , screenDirection)
+upDirection = np.array([0,0,1])
+horizon = np.array([0, 1, 0])
+boundsTranslation = np.array([0, 0, 0])  # target
+screenTranslation = np.array([0, 1, 0])  # screen behind the target
+
+cams = mgo.createCamsCirc(numViews , camsRadius , camsHeight , upDirection , boundsTranslation , archAngleSize , horizon)
 
 
 ## BUILD AND SHOW SCENE
-if do_plot: 
-    #scene = nbv.Scene(boundsPLYPath)
-    scene = nbv.Scene(boundsPLYPath, [0, 0, 0], screenPLYPath, [0, 1, 0])
+if do_plot:
+    scene = nbv.Scene(boundsPLYPath , boundsTranslation , screenPLYPath , screenTranslation)
     scene.addCam(cams)
     #scene.addLight(lights)
     camScale = 0.2
@@ -51,31 +55,37 @@ if do_plot:
 mitsuba = mitLib.Mitsuba(base_path,scene_name,params)
 mitsuba.SetSunSky(np.array([[3, 3,300, 0,0,0, 0,0,1]]))
 
-result_image = np.zeros((cams.shape[0],), dtype=np.object)
-#result_image = [[] for i in range(cams.shape[0])]
-for i in range(cams.shape[0]):
-    
-    #mitsuba.SetCamera(cams[i][None,:])
-    mitCam = mgo.rotScene2Mitsuba(cams[i][None,:])
+# Rendering the scene
+simIm = np.zeros((cams.shape[0],), dtype=np.object)
+for i, cam in enumerate (cams):
+    mitCam = mgo.rotScene2Mitsuba(cam[None,:])
     mitsuba.SetCamera(mitCam[None,:])    
     #light_curr = np.hstack((np.random.random([1,3])+cams[i][0:3],-cams[i][0:3][None,:]+cams[i][3:6][None,:],up_dir[None,:]))
     #mitsuba.SetSpotlight(light_curr)
-    result_image[i] = mitsuba.Render(params['sampleCount'])
-    print 'Rendered '+str(i)+'/'+str(cams.shape[0])
+    simIm[i] = mitsuba.Render(params['sampleCount'])
+    print 'Rendered '+str(i + 1)+'/'+str(numViews)
 
 ## PLOT RESULT
 if 1:
-    for i in range(len(result_image)):
-        plt.subplot(1,cams.shape[0],i+1)
-        plt.imshow(result_image[i],vmin=0.0,vmax=5.0)
+    for i, im in enumerate(simIm):
+        plt.subplot(1,numViews,i+1)
+        plt.imshow(im,vmin=0.0,vmax=5.0)
         plt.axis('off')
     plt.show()
 
 print 'Done rendering'
 
-save_file_name = raw_input('Enter file name: ')
-
-scipy.io.savemat(r'C:\Users\addalin\mitsuba_sim\opticalprobing'.replace('\\', '/') + "/" + save_file_name+'_pyCams.mat'  , mdict={'cams': cams})
-scipy.io.savemat(r'C:\Users\addalin\mitsuba_sim\opticalprobing'.replace('\\', '/') + "/" + save_file_name+'_pyImages.mat', {'result_image':result_image})
-
-
+resTime = strftime("%d-%m-%Y_%H-%M-%S", gmtime())
+saveRes =  raw_input('Do you want to save resutls [ "y" | "n" ]: ')
+if saveRes == 'y':
+    
+    save_file_name = scene_name + "_" + resTime #raw_input('Enter file name: ')
+    sceneParams = {'nViews': numViews, 'camsRadius': camsRadius, 'camsHeight': camsHeight, 'archAngleSize': archAngleSize, 'upDirection': upDirection,
+                   'horizon': horizon, 'boundsTranslation': boundsTranslation, 'screenTranslation': screenTranslation,}
+    
+    scipy.io.savemat(resultsPath + "/" + save_file_name +'_pyCams.mat'  , mdict={'cams': cams , 'camMitsubaParam': params, 'sceneParams': sceneParams})
+    scipy.io.savemat(resultsPath + "/" + save_file_name +'_pyImages.mat', {'result_image':simIm})
+    
+    print resTime, " results are saved at: ", resultsPath
+    
+    
