68d7aabe488bebb655c1e03a59cc233ec03857f1
Addalin <adi.vainiger@gmail.com>
2017-08-24 18:00:45 +0300

    fix nbv file that was mistakenly writen as mitsubaWarapperLib

--- nextBestViewLib.py
+++ nextBestViewLib.py
@@ -1,228 +1,100 @@
 import os, sys
 
-"""  mitsubaWrapperLib.py - This lib is used to operate Mitsuba  """ 
-
-mitsuba_path = os.environ['MITSUBA_PATH'].replace('\\', '/')
-#mitsuba_path = 'C:/Users/addalin/Mitsuba 0.5.0 64bit/Mitsuba 0.5.0'
-#mitsuba_path = '/home/addalin/Softwares/Mitsuba/dist'#'C:/Users/addalin/Mitsuba 0.5.0 64bit/Mitsuba 0.5.0'
-
-sys.path.append(mitsuba_path + '/python/2.7')
-
-# Ensure that Python will be able to find the Mitsuba core libraries
-os.environ['PATH'] = mitsuba_path + os.pathsep + os.environ['PATH']
-
-import mitsuba
-from mitsuba.core import *
-from mitsuba.render import SceneHandler
-from mitsuba.render import RenderQueue, RenderJob
-from mitsuba.render import Scene
-import multiprocessing
-#import cv2
 import numpy as np
+import mayavi.mlab as mi
+import matplotlib.pyplot as plt
+import parseVolumeDada as pvd
+import miscGeometry as mgo
 
-
-# Each row is a cam & light location in Mitsuba's LookAt format,
-# i.e. [Point(position) Point(looking towards) Vector(up direction)].
-# For example: [1,2,3,  0,0,0,  0,0,1] is a cam/light positioned at [1 2 3], looking 
-# towards [0 0 0] with an up vector of [0,0,1]
-
-
-class Mitsuba(object):
-
+class Scene(object):
+    
     # Constructor
-    def __init__(self, base_path,scene_name,params):    
+    def __init__(self, boundsFilename , boundsTrans , screenFilename , screenTrans ):
+        self.boundsVertices, self.boundsFaces = pvd.ply2Poly(boundsFilename, boundsTrans)
+        self.screenVertices, self.screenFaces = pvd.ply2Poly(screenFilename, screenTrans)
+        self.screenVertices = self.screenVertices.dot(mgo.rotX(90))
+        self.cams = None
+        self.lights = None
+    
+    # Add camera
+    def addCam(self,obj):
+        if self.cams==None:
+            self.cams = obj
+        else:
+            self.cams = np.vstack((self.cams,obj))
+            
+    # Add Light
+    def addLight(self,obj):
+        if self.lights==None:
+            self.lights = obj
+        else:
+            self.lights = np.vstack((self.lights,obj))        
 
-        self.params = params
-        self.light = []
-        # Get a reference to the thread's file resolver
-        self.fileResolver = Thread.getThread().getFileResolver()
-        scenes_path = base_path + '/' + scene_name + '/mitsuba' 
-        self.fileResolver.appendPath(scenes_path)
-        paramMap = StringMap()
-        paramMap['myParameter'] = 'value'
-        # Load the scene from an XML file
+    # Get faces
+    def getFaces(self):
+        return self.faces
+    
+    # Get vetecies
+    def getVert(self):
+        return self.vertex
+    
+    # Show system
+    def showSystem(self,do_show=False,camScale=1.0):
+        mi.triangular_mesh(self.boundsVertices[:,0],self.boundsVertices[:,1],self.boundsVertices[:,2],self.boundsFaces)
+        mi.triangular_mesh(self.screenVertices[:,0],self.screenVertices[:,1],self.screenVertices[:,2],self.screenFaces)
+        
+        # Plot cameras
+        if self.cams is not None:
+            shape_points = camScale * np.array([[0, 0, 0],[1 ,1 ,1],[1 ,-1 ,1],[-1 ,-1 ,1],[-1 ,1 ,1],[1,1,1],[1 ,-1 ,1],[0,0,0],[-1 ,1 ,1],[-1 ,-1 ,1],[0,0,0]])
+            for i in range(self.cams.shape[0]):
+                self.__plotObj(self.cams[i,:],shape_points,(1,0,0),'Cam'+str(i))
+        
+        # Plot lights
+        if self.lights is not None:
+            radius = 1
+            base_height = 3
+            lamp_ang = np.linspace(0,2*np.pi,50)[:,None]
+            base_points = np.hstack( (np.sin(lamp_ang)*radius , np.cos(lamp_ang)*radius ))
+            base_points = np.hstack( (base_points , base_height*np.ones([base_points.shape[0],1]) ))
+            lamp_ang = np.array([0 , np.pi/2 , np.pi, 3*np.pi/2])[:,None]
+            cone_points = np.zeros([np.size(lamp_ang)*2,3])    
+            cone_points[1::2,:] = np.hstack( (np.sin(lamp_ang)*radius ,
+                                              np.cos(lamp_ang)*radius ,  
+                                              base_height * np.ones([cone_points.shape[0]/2,1]))) 
+            shape_points = np.vstack((base_points , cone_points ))
+            
+            for i in range(self.lights.shape[0]):
+                self.__plotObj(self.lights[i,:],shape_points,(1,1,0),'Light'+str(i))
+        if do_show:        
+            mi.show()        
 
-        self.scene = SceneHandler.loadScene(self.fileResolver.resolve(scene_name + '.xml'), paramMap)
-
-        # Setting & adding emmiters to scene - diffusive screen, created out of multiple sub-screens
-        self.SetWideScreen()
-        #self.SetWideScreen(params['screenWidth'] , params['screenHeight'],params['resXScreen'],params['resYScreen'], params['screenZPos'],params['variantRadiance'])                
-        self.addSceneLights()
-
-        #Simultaneously rendering multiple versions of a scene
-        self.scene.initialize()
-        self.scheduler = Scheduler.getInstance()
-        # Start up the scheduling system with one worker per local core
-        for i in range(0, multiprocessing.cpu_count()):
-            self.scheduler.registerWorker(LocalWorker(i, 'wrk%i' % i))
-        self.scheduler.start()
-
-        # Create a queue for tracking render jobs
-        self.queue = RenderQueue()
-        self.sceneResID = self.scheduler.registerResource(self.scene)
-
-    # ----------------------SET SUNSKY -----------------------
-    def SetSunSky(self, dir_vec, radiance=1):
-        pmgr = PluginManager.getInstance()
-        obj = pmgr.create({
-                    'type' : 'sun',
-                    'radiance' : Spectrum(radiance)
-                })
-        self.light.append(obj)
-
-    # ----------------------SET SPOTLIGHT -----------------------
-    def SetSpotlight(self, dir_vec):
-        pmgr = PluginManager.getInstance()
-        obj = pmgr.create({
-                    'type' : 'spot',
-                    'cutoffAngle' : 40.0,
-                    'intensity' : Spectrum(50),
-                    'toWorld' : Transform.lookAt(
-                        Point( dir_vec[0,0] , dir_vec[0,1] , dir_vec[0,2]),
-                        Point( dir_vec[0,3] , dir_vec[0,4] , dir_vec[0,5]),
-                        Vector(dir_vec[0,6] , dir_vec[0,7] , dir_vec[0,8])
-                    )
-                })
-        self.light.append(obj)
-
-    # ----------------------SET SCREEN -----------------------
-    def SetRectangleScreen(self, screenPos, radiance, dx, dy):
-        """Set a sub-screen (rectangle shape attached to area emitter) at screenPos, with dimentions of [dx X dy],
-        With radiance radiance [Watt/(m^2*sr)] """                
-        pmgr = PluginManager.getInstance()
-        resctScreen = pmgr.create({
-                    'type' : 'rectangle',
-                    'bsdf': {
-                        'type': 'diffuse',
-                            'illuminant':Spectrum(1.0)
-                            #'reflectance' : Spectrum(0.78) # this is causing peaks noise
-
-                            },
-                    'toWorld' : Transform.translate(Vector (screenPos[0], screenPos[1], screenPos[2])) * Transform.rotate (Vector(1, 0,0), 180.0) * Transform .scale(Vector(dx/2, dy/2, 1)),
-                    'emitter': {
-                        'type': 'constant',#'area',
-                            'radiance':Spectrum(radiance),
-                            'samplingWeight':10.0                            
-                    }
-                })
-        self.light.append(resctScreen)                                    
-    # ----------------------SET WIDE SCREEN -----------------------       
-    def SetWideScreen(self):
-    #def SetWideScreen(self, width = 50.0 , height = 20.0, resX = 1, resY = 1, screenZPos = 2, rand = False):        
-        """Set a screen of light at Z = screenZPos, with dimentions of [width X height], containing [resX X resY] sub-surfaces of screens.
-        The radiance [Watt/(m^2*sr)] of screeen can be either constant [1] of variant unifomily [0,1] """
-        width = self.params['screenWidth']
-        height = self.params['screenHeight']
-        resX = self.params['resXScreen']
-        resY = self.params['resYScreen']
-        screenZPos = self.params['screenZPos']
-        rand = self.params['variantRadiance']  
-
-        screenXCorners = width/2* np.array([-1 , 1])
-        screenYCorners = height/2*np.array([-1 , 1])
-        dx = width / resX
-        dy = height / resY
-
-        screenX = np.linspace( screenXCorners [0] + dx / 2, screenXCorners [1] - dx / 2, num=resX)
-        screenY = np.linspace( screenYCorners [0] + dy / 2, screenYCorners [1] - dy / 2, num=resY)
-        for x in screenX:
-            for y in screenY:
-                curRadiance = np.random.uniform(0.0, 1.0) if rand else 1.0
-                self.SetRectangleScreen( np.array([x, y, screenZPos]), curRadiance, dx, dy)
-    #def SetWideScreen(self):
-        #"""Set a screen of light at Z = screenZPos, with dimentions of [width X height], containing [resX X resY] sub-surfaces of screens.
-        #The radiance [Watt/(m^2*sr)] of screeen can be either constant [1] of variant unifomily [0,1] """
-        #width = self.params['screenWidth']
-        #height = self.params['screenHeight']
-        #resX = self.params['resXScreen']
-        #resY = self.params['resYScreen']
-        #screenZPos = self.params['screenZPos']
-        #rand = self.params['variantRadiance']                
-        #self.SetWideScreen() = SetWideScreen(width  , height , resX , resY, screenZPos, rand )        
-
-    # ----------------------SET CAMERA -----------------------
-    def SetCamera(self,dir_vec):
-        """Create and pre-set a new sensor according to dir_vec""" 
-        pmgr = PluginManager.getInstance()
-        obj = pmgr.create({
-                    'type' : 'perspective',
-                    'toWorld' : Transform.lookAt(
-                        Point( dir_vec[0,0] , dir_vec[0,1] , dir_vec[0,2]),
-                        Point( dir_vec[0,3] , dir_vec[0,4] , dir_vec[0,5]),
-                        Vector(dir_vec[0,6] , dir_vec[0,7] , dir_vec[0,8])
-                        ),
-                    #'focalLength': self.params['focalLength'],
-                    'fov': self.params['fov'],
-                    'fovAxis': self.params['fovAxis'],                    
-                    'film' : {
-                        'type' : 'hdrfilm',  #'mfilm',  #'ldrfilm',
-                        'width' :  self.params['camWidth'],
-                        'height' : self.params['camHeight'],
-                        },
-                    'sampler' : {
-                        'type' :'ldsampler',#'independent',#'ldsampler',
-                               'sampleCount' : self.params['sampleCount'],
-                               'dimension': self.params['samplerDimention']
-                               #'scramble':10
-                               },
-                    'medium' : {
-                       'type' : 'homogeneous',
-                                'id': 'underwater',
-                                'scale': 0.5, 
-                                'sigmaS' : Spectrum([0.4, 0.3, 0.3]),  #[0.02, 0.02, 0.02]),
-                                'sigmaA' : Spectrum([0.45, 0.06, 0.05]),  #[0.3, 0.3, 0.3]),
-                                'phase' : {
-                                    'type' : 'hg',
-                                        'g' : 0.9
-                                }    
-
-                   }                            
-                }) 
-        self.cam = obj
-
-    def createSampler(self,sampleCount):
-        pmgr = PluginManager.getInstance()
-        obj = pmgr.create({
-                    'type' : 'ldsampler',#''independent',#'sobol',#'ldsampler',  #',
-                    'sampleCount' : sampleCount,
-                    #'sampleCount' : self.params['sampleCount'],
-                    'dimension': self.params['samplerDimention'] #,
-                    #'scramble':10
-                })
-        self.sampler = obj        
-
-    # ----------------------RENDER-----------------------         
-    def addSceneLights(self):
-        """ Adding all the pre-setted lights to the scene"""
-        currScene = Scene(self.scene)
-        for light in self.light:
-            currScene.addChild(light)  
-        self.scene = currScene
-
-    # ----------------------RENDER-----------------------         
-    def Render(self,sampleCount,i):
-        currScene = Scene(self.scene) 
-        currScene.configure()
-        pmgr = PluginManager.getInstance()
-        currScene.addSensor(self.cam)   
-        currScene.setSensor(self.cam) 
-        self.createSampler(sampleCount)
-        currScene.setSampler(self.sampler) #O to sleep :-) who is it?!!? the void hahahah AMIT ?!?!(self.sampler)
-
-        currScene.setDestinationFile('')
-        # Create a render job and insert it into the queue
-        #job = RenderJob('myRenderJob'+str(i), currScene, self.queue )
-        job = RenderJob('myRenderJob'+str(i), currScene, self.queue,self.sceneResID )
-        job.start()
-
-        self.queue.waitLeft(0)
-        self.queue.join()
-
-        film = currScene.getFilm()
-        size = film.getSize()
-        bitmap = Bitmap(Bitmap.ERGBA, Bitmap.EFloat16, size)
-        film.develop(Point2i(0, 0), size, Point2i(0, 0), bitmap)
-        # End of render - get result
-        result_image = np.array(bitmap.buffer())                                
-        currSceneInfo = currScene.getAABB
-        return result_image, currSceneInfo
+    #  -------------- Private methods --------------        
+    
+    # Plot objera        
+    def __plotObj(self,obj,obj_shape,base_color,obj_name):
+        # objera center
+        mi.points3d(obj[0],obj[1],obj[2],color=base_color,scale_factor=0.2) 
+        mi.text3d(  obj[0],obj[1],obj[2],obj_name,scale=0.3,color=(0,0,0))
+        # Transform 
+        T,T_inv = mgo.transformLookAt(obj[0:3],obj[3:6],obj[6:9])
+        obj_shape = np.dot(T,np.r_[ 
+            obj_shape.transpose(),
+            np.ones([1,np.shape(obj_shape)[0]])])
+        mi.plot3d(obj_shape[0,:],obj_shape[1,:],obj_shape[2,:],tube_radius=None,reset_zoom=False)
+       
+        
+                
+    
+if __name__=='__main__':
+    print 'NBV file'
+    scene_name = 'hetvol'
+    mitsuba_sim_path = os.environ['MITSUBA_SIM'].replace('\\', '/')
+    scene_base_path    = mitsuba_sim_path + '/3D_models'      
+    scene = Scene(scene_base_path + scene_name + '/' + scene_name + '.ply')
+    cams    = np.array([[7,-6,5, 0,0,0,  0,0,1]])
+    lights  = np.array([[3,0,0 , 0,0,0,  0,0,1]])
+    scene.addCam(cams)
+    scene.addLight(lights)
+    scene.showSystem()
+    
+    print 'end NBV'
